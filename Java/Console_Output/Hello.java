8b4268802e718e7c56505059a55d780a5a4f61cb
Misha Ward <wardm5@uw.edu>
2019-07-04 19:01:41 -0700

    Create Hello.java

---
+++ Java/Console_Output/Hello.java
@@ -0,0 +1,7 @@
+//  Write a console program in a class named Hello that prints the following console output:  Hello, world!
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
