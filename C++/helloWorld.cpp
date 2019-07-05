81de431e65bd9d06b30a0890c79df3cd24364651
Misha Ward <wardm5@uw.edu>
2019-07-04 17:26:16 -0700

    Create helloWorld.cpp

---
+++ C++/helloWorld.cpp
@@ -0,0 +1,6 @@
+//  Write a complete C++ program with a main function that prints the following console output:  Hello, world!
+
+int main () {
+    cout << "Hello, world!" << endl;
+    return 0;
+}
